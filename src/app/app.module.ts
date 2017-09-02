4c2ea8f6ab45a8134680e5545bece82cd3324255
Jie Rong (Felix) <rainmore24@gmail.com>
2017-09-03 09:42:32 +1000

    The first tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
